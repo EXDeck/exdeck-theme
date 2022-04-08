348db0f968b74a6a3530fdea245666c79c1f658f
hakunagi <hakunagi.rei@gmail.com>
2022-04-08 21:58:54 +0900

    色々な修正

--- gulpfile.js
+++ gulpfile.js
@@ -1,12 +1,37 @@
 const gulp = require("gulp")
 const gulpDartSass = require("gulp-dart-sass")
 const sassGlob = require("gulp-sass-glob-use-forward")
+const rename = require("gulp-rename")
+const cleanCSS = require("gulp-clean-css")
+const header = require("gulp-header")
 
-const sass = () => {
+const themeName = "harmony"
+
+const pack = require("./package.json")
+
+const licenseTexts = [
+  `${pack.name} v${pack.version}`,
+  `${pack.license} License`,
+  "https://github.com/MarinDeck/birdseye-theme-harmony",
+]
+
+const build = () => {
   return gulp
     .src("src/**/*.scss")
     .pipe(sassGlob())
     .pipe(gulpDartSass({ includePaths: ["src"], outputStyle: "compressed" }))
+    .pipe(
+      rename({
+        basename: themeName + ".min",
+      })
+    )
+    .pipe(header(`/*! ${licenseTexts.join(" | ")} */`))
+    .pipe(cleanCSS())
     .pipe(gulp.dest("dist/"))
 }
-exports.sass = sass
+exports.build = build
+
+const watch = () => {
+  return gulp.watch("src/**/*.scss", gulp.series(["build"]))
+}
+exports.watch = watch
